7cbbf33ea3483bf300f1fa4eebaeba9d412e347b
aakKel <952057534@qq.com>
2023-02-09 23:40:17 +0800

    修改kalloc ，kfree，增加自旋锁结构

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -14,6 +14,21 @@ void freerange(void *pa_start, void *pa_end);
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
 
+
+
+// 为每个物理页增加引用计数，在最后一个pte销毁后释放该物理页
+struct pp_ref {
+    struct spinlock lock;
+    // 最大物理地址 / 每页大小 -> 为每个物理页创建一个映射
+    int cnt[PHYSTOP / PGSIZE];
+
+} ref;
+
+int kref_cnt(void *pa) {
+    //获取引用计数。
+    return ref.cnt[(uint64)pa / PGSIZE];
+}
+
 struct run {
   struct run *next;
 };
@@ -27,6 +42,8 @@ void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+  //初始化锁
+    initlock(&ref.lock,"ref");
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -46,20 +63,24 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
-  struct run *r;
+    struct run *r;
+    if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+        panic("kfree");
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
-    panic("kfree");
+    acquire(&ref.lock);
+    //引用计数为0才释放.
+    if (--ref.cnt[(uint64)pa / PGSIZE] == 0) {
+        release(&ref.lock);
+        r = (struct run*)pa;
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
-
-  r = (struct run*)pa;
-
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+        memset(pa,1,PGSIZE);
+        acquire(&kmem.lock);
+        r -> next = kmem.freelist;
+        kmem.freelist = r;
+        release(&kmem.lock);
+    } else {
+        release(&ref.lock);
+    }
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -68,15 +89,20 @@ kfree(void *pa)
 void *
 kalloc(void)
 {
-  struct run *r;
+    struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    acquire(&kmem.lock);
+    r = kmem.freelist; // 获取内存
+    if(r) {
+        kmem.freelist = r->next; // 从空闲链表中删除获取的内存
+        acquire(&ref.lock);
+        ref.cnt[(uint64)r / PGSIZE] = 1;  // 将引用计数初始化为1
+        release(&ref.lock);
+    }
+    release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+    if(r)
+        memset((char*)r, 5, PGSIZE); // fill with junk
+    return (void*)r;
+
 }
